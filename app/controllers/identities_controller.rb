aa1ed3086a54c1c31bd61d60395daa050b4a57c0
Ying Wu <ying.wu@pbrc.edu>
2019-08-30 09:56:35 -0500

    login is not required to approve/disapprove new accounts

--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -19,7 +19,7 @@
 # TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 class IdentitiesController < ApplicationController
-  before_action :authenticate_identity!
+  #before_action :authenticate_identity!
 
   def approve_account
     respond_to :html
